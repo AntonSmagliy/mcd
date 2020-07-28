e21423336ddb6cbcfcd77825265e689f556eee54
AntonSmagliy <mxel98@gmail.com>
2020-07-28 20:15:04 +0300

    both pages + responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log(123)
+$().ready(function () {
+    $('#slider-wrapper').slick({
+        slidesToShow: 3,
+        prevArrow: '<div class="prevArrow"><img src="img/sliderarrowrever.png" alt=""></div>',
+        nextArrow: '<div class="nextArrow"><img src="img/sliderarrow.png" alt=""></div>',
+        responsive: [
+            {
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+})
